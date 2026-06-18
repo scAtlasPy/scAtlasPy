8ca8afb21b952c164ee98aaea340536c4ccacb8f
yyz1996 <940937734@qq.com>
2026-06-18 16:46:13 +0800

    补充版本号

--- Package/python-version-scAtlasAnalysis/scatlaspy/__init__.py
+++ Package/python-version-scAtlasAnalysis/scatlaspy/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from . import data
 from . import preprocessing as pp
 from . import tools as tl
@@ -5,4 +7,7 @@ from . import plots as pl
 
 from .data import Atlas, set_verbosity
 
-
+try:
+    __version__ = version("scatlaspy")
+except PackageNotFoundError:
+    __version__ = "unknown"
